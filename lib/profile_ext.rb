39f14c3b9d680150c3974a2cc700ecc99ad0d5fd
jason gagne <jgagne33@gmail.com>
2010-09-07 15:52:09 -0700

    adding a method to fix imports

--- lib/profile_ext.rb
+++ lib/profile_ext.rb
@@ -22,32 +22,32 @@ module ProfileExt
       self.person.person_group_ids = self.person.person_group_ids << group2.id
       self.save
     end
-  end
-  def unconfirmed
-    if self.respond_to?(:confirmed) 
-      self.confirmed.blank?
-    else
-      self.person.confirmed.blank?
-    end
-  end
-  def group_membership_check
-    ids_for_person = self.person_group_ids
-    for state in ["confirmed", "unconfirmed"]
-      group = PersonGroup.find_or_create_by_title("#{state.capitalize} Profiles", :public => false)
-      group.update_attributes(:public => false) if group.public
-      if self.send(state.downcase.to_sym)
-        self.person_group_ids = self.person_group_ids << group.id
-      end
-      unless self.send(state.to_sym)
-        self.person_group_ids = self.person_group_ids.reject{|g| g == group.id}
+    def unconfirmed
+      if self.respond_to?(:confirmed) 
+        self.confirmed.blank?
+      else
+        self.person.confirmed.blank?
       end
     end
-    self.save unless ids_for_person == self.person_group_ids
-  end
-  def check_for_invalid_imports
-    if !self.new_record? && self.created_at.blank?
-      self.last_name ||= "Guest"
-      self.created_at = Person.find(self.id + 1).created_at - 5.minutes
+    def group_membership_check
+      ids_for_person = self.person_group_ids
+      for state in ["confirmed", "unconfirmed"]
+        group = PersonGroup.find_or_create_by_title("#{state.capitalize} Profiles", :public => false)
+        group.update_attributes(:public => false) if group.public
+        if self.send(state.downcase.to_sym)
+          self.person_group_ids = self.person_group_ids << group.id
+        end
+        unless self.send(state.to_sym)
+          self.person_group_ids = self.person_group_ids.reject{|g| g == group.id}
+        end
+      end
+      self.save unless ids_for_person == self.person_group_ids
+    end
+    def check_for_invalid_imports
+      if !self.new_record? && self.created_at.blank?
+        self.last_name ||= "Guest"
+        self.created_at = Person.find(self.id + 1).created_at - 5.minutes
+      end
     end
   end
 end
